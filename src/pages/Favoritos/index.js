d320b4daa5a0e6391acd112271037a7a62f1fd26
GMDorneles <gabrielly.maldonadoo@gmail.com>
2021-06-28 11:03:13 -0400

    final commit

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom';
 export default function Favoritos(){
+        const [filmes,setFilmes]=useState([]);
+        useEffect(()=>{
+            const minhaLista= localStorage.getItem('filmes');
+            setFilmes(JSON.parse(minhaLista)||[]);
+        },[]);
+        function handleDelete(id){
+           ///devilve tds os itens menos  o clicado
+           let filtroFilmes = filmes.filter((item)=>{
+                return(item.id!==id);//reotna com filtro item diferente do clicado  
+           }) 
+           setFilmes(filtroFilmes);
+           localStorage.setItem('filmes',JSON.stringify(filtroFilmes));
+        }
+    
     return(
-        <div>
-            <h1>Página de Favoritos</h1>
+        <div id='meus-filmes'>
+            <h1>Meus filmes</h1>
+            {filmes.length===0 && <span>voce nao possui filmes</span>}
+            <ul>
+                {filmes.map((item)=>{
+                    return(
+                        <li key={item.id}>
+                            <span>{item.nome}</span>
+                            <div>
+                                <Link to={`/filme/${item.id}`}>ver detalhe</Link>
+                                <button onClick={()=>handleDelete(item.id)}>Excluir</button>{/*funçao anonima faz funciona apensa quando clicado*/}
+                            </div>
+                        )
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Filme from './pages/Filme';
 import Favoritos from './pages/Favoritos';
+import Erro from './pages/Erro';
 
 const Routes =()=>{
     return(
@@ -12,6 +13,7 @@ const Routes =()=>{
                 <Route exact path='/' component={Home}/>
                 <Route exact path='/filme/:id' component={Filme} />
                 <Route exact path='/favoritos' component={Favoritos}/>
+                <Route path='*' component={Erro}/>
             </Switch>
         </BrowserRouter>
     )
